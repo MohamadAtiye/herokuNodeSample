b4211d4e9d73c16b60e5417011caed204261b42b
DESKTOP-8PL7DR1\moham <mohamad.atiye@gmail.com>
2018-05-26 01:11:51 +0300

    no message

--- firstappp.js
+++ firstappp.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var dt = require('./returnDate.js');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+	res.write('Hello World!, time is '+ dt.myDateTime());
+    res.end();
 }).listen(8080);
